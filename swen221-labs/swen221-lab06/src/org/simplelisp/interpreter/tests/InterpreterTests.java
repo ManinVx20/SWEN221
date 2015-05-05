3032f9b2152d3adcf01e3cd08aa23bb570349e22
Diego Trazzi <diegotrazzi@gmail.com>
2015-05-06 10:47:44 +1200

    Lab 06 few more tests

--- swen221-labs/swen221-lab06/src/org/simplelisp/interpreter/tests/InterpreterTests.java
+++ swen221-labs/swen221-lab06/src/org/simplelisp/interpreter/tests/InterpreterTests.java
@@ -1,7 +1,7 @@
 package org.simplelisp.interpreter.tests;
 
 import static org.junit.Assert.*;
-
+import java.io.FileNotFoundException;
 import org.junit.Test;
 import org.simplelisp.interpreter.*;
 
@@ -18,19 +18,75 @@ public class InterpreterTests {
 		try {
 			new Interpreter().type_check("test", new LispExpr[]{});
 			new Interpreter().type_check("test2", new LispExpr[]{new LispInteger(3),new LispInteger(0), new LispInteger(1), new LispInteger(10), new LispInteger(1), new LispInteger(1), new LispInteger(1)},LispExpr.class);
-			LispInteger li = new LispInteger(0);
-			li.value();
-			li.equals(0);
-			li.toString();
-			LispExpr li2 = new LispNil();
-			LispExpr li3 = new LispChar('d');
-			
+//			LispInteger li = new LispInteger(0);
+//			li.value();
+//			li.equals(0);
+//			li.toString();
+//			LispExpr li2 = new LispNil();
+//			LispExpr li3 = new LispChar('d');
 //			new Interpreter().type_check("testX", new LispExpr[]{},LispExpr.class);			
 		} catch (Error e) {
 			fail("Error in type checking: " + e.getMessage());
 		} 
 		//TODO test for non-empty input
 	}
+	
+	@Test public void testType_sum() {
+		try {
+			Interpreter interpreter = new Interpreter();
+			LispExpr root = Parser.parse("(+ 2 3)");
+			LispExpr result = interpreter.evaluate(root);
+			assertEquals("5", result.toString());
+		} catch (Error e) {
+			fail("Error in type checking: " + e.getMessage());
+		} 
+	}
+	
+	@Test public void testType_divisionByZero() {
+		try {
+			Interpreter interpreter = new Interpreter();
+			LispExpr root = Parser.parse("(/ 2 0)");
+			LispExpr result = interpreter.evaluate(root);
+			System.out.println(result.toString());
+			assertEquals("5", result.toString());
+			fail("this should have errored");
+		} catch (ArithmeticException e) {
+			// the super-hero catches the monster
+		} 
+	}
+	
+	@Test public void testType_print() {
+		try {
+			Interpreter interpreter = new Interpreter();
+			LispExpr root = Parser.parse("'(+ 1 2)");
+			LispExpr result = interpreter.evaluate(root);
+			assertEquals("(+ 1 2)", result.toString());
+		} catch (Error e) {
+			fail("Error in type checking: " + e.getMessage());
+		} 
+	}
+	
+	@Test public void testType_car() {
+		try {
+			Interpreter interpreter = new Interpreter();
+			LispExpr root = Parser.parse("(car '(1 2 3))");
+			LispExpr result = interpreter.evaluate(root);
+			assertEquals("1", result.toString());
+		} catch (Error e) {
+			fail("Error in type checking: " + e.getMessage());
+		} 
+	}
+
+	
+	@Test public void testType_load() {
+		try {
+			Interpreter interp = new Interpreter();
+			interp.load("hello.sl");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+		
 	
 	
 	
--- swen221-labs/swen221-lab06/src/org/simplelisp/interpreter/tests/LispInternalFunctionsTests.java
+++ swen221-labs/swen221-lab06/src/org/simplelisp/interpreter/tests/LispInternalFunctionsTests.java
@@ -3,14 +3,17 @@ package org.simplelisp.interpreter.tests;
 import org.junit.Test;
 import org.simplelisp.interpreter.InternalFunctions;
 import org.simplelisp.interpreter.Interpreter;
+import org.simplelisp.interpreter.LispFunction;
 
 
 public class LispInternalFunctionsTests {
 	
 	@Test
 	public void  testConstructors(){
+		Interpreter interp = new Interpreter();
 		InternalFunctions internalF = new InternalFunctions();
-		internalF.setup_internals(new Interpreter());
+		internalF.setup_internals(interp);
+//		interp.setGlobalExpr("print", new LispFunction());
 	}
 	
 }
