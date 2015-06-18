542c2ad5179e673cc32c299c25809d86c6083369
Diego Trazzi <diegotrazzi@gmail.com>
2015-06-18 21:32:42 +1200

    Hacking Java

--- swen221-generic/L19_Reflection/TypeCheck.java
+++ swen221-generic/L19_Reflection/TypeCheck.java
@@ -33,7 +33,7 @@ public class TypeCheck {
 		}
 		
 		// grab the first field, the private one
-		Field f = fields[0];
+		Field f = tClass.getDeclaredField("secretField");
 		// set accessible true (necessary for private fields)
 		f.setAccessible(true);
 		
