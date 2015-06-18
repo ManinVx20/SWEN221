c88769b67974c5285a6c7203b7b6f0265dd175ec
Diego Trazzi <diegotrazzi@gmail.com>
2015-06-18 21:28:06 +1200

    Hacking Java

--- swen221-generic/L19_Reflection/TypeCheck.java
+++ swen221-generic/L19_Reflection/TypeCheck.java
@@ -12,41 +12,35 @@ class TestClass {
 
 public class TypeCheck {
 	public static void main(String args[]) throws NoSuchFieldException, IllegalAccessException {
-		// String test = new String("ssss");
-		// Class<? extends String> c1 = test.getClass();
-		// Class<?> c2 = test.getClass();
-		// System.out.println(c2);
-		// System.out.println(test.getClass());
-		// System.out.println(String.class);
-		// This is like python, when you call dir(cmd)
-		// Object o = test;
-		// Class<?> oClass = o.getClass();
-		// System.out.println(oClass);
-		// System.out.println(String.class);
-		// Method ms[] = oClass.getDeclaredMethods();
-		// for (Method m:ms){
-		// 	System.out.println("Method: "+m.getName());
-		// }
-		// --------------------------------------------
 		// make an instance of the secret method in testClass
 		TestClass t = new TestClass();
+		
 		// t.secretMethod(); // this doesn't compile because the method is private
-		Class<?> tClass = t.getClass(); // get the type of class
+		
+		// bu we can get the type of the class ... then hack it
+		Class<?> tClass = t.getClass(); 
+		
+		// find out what methods the class has 
 		Method[] methods = tClass.getDeclaredMethods();
 		for (Method m : methods){
 			System.out.println(m);
 		}
+		
+		// find out which field has
 		Field[] fields = tClass.getDeclaredFields();
 		for (Field f : fields){
 			System.out.println(f);
 		}
+		
 		// grab the first field, the private one
 		Field f = fields[0];
+		// set accessible true (necessary for private fields)
 		f.setAccessible(true);
+		
 		System.out.println("Value of secret field: "+f.get(t));
-		f.set(t,10); // set the secret field to 10
+		// change the vlaue of private field
+		f.set(t,10);
 		System.out.println("Value of secret field after hack: "+f.get(t));
 		System.out.println("-------");
-		// f.set();
 	}
 }
